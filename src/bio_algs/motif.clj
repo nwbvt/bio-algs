1e2ec89ef78c03aeb15ad06d807db2857cfd3b58
Nick Brown <nwbrown@gmail.com>
2014-11-30 20:46:19 -0500

    Greedy Motif Search and related functions

--- src/bio_algs/motif.clj
+++ src/bio_algs/motif.clj
@@ -1,6 +1,7 @@
 (ns bio-algs.motif
   (:require [bio-algs.ori-rep :refer [varients hamming-dist]]
-            [clojure.set :refer :all]))
+            [clojure.set :refer :all]
+            [clojure.string :refer [split trim]]))
 
 (defn matches?
   "Returns true iff the dna contains the pattern"
@@ -61,6 +62,70 @@
    A median kmer is defined as the one that minimizes the distance between it an all dna strings"
   [k dna]
   (let [kmers (apply union (map (partial all-kmers k) dna))]
-    (println kmers)
-    (println (map #(dist % dna) kmers))
     (apply min-key #(dist % dna) kmers)))
+
+(defn score-kmer
+  "Scores a kmer against a given profile"
+  [kmer profile]
+  (apply * 
+         (for [i (range (count kmer))
+               :let [a (nth kmer i)]]
+           (nth (profile a) i))))
+
+(defn most-probable-kmer
+  "Finds the most probably kmer in the given dna fitting the given pattern"
+  [k dna profile]
+  (apply max-key #(score-kmer % profile) (all-kmers k dna)))
+
+(defn make-profile-from-string
+  "Makes a profile from the input string format"
+  [input]
+  (let [strrows (split input #"\n")
+        rows (map (fn [r] (map #(Double/parseDouble %) (split (trim r) #" "))) strrows)]
+    (zipmap [\A \C \G \T] rows)))
+
+(defn score
+  "Score the motifs given"
+  [motifs]
+  (let [n (count (first motifs))]
+   (apply +
+         (for [i (range n)
+               :let [xs (map (partial #(nth % i)) motifs)
+                     freqs (frequencies xs) 
+                     consensus (apply max-key freqs (keys freqs))]]
+           (count (filter (partial not= consensus) xs))))))
+
+(defn gen-profile
+  "Generates a profile from a set of motifs"
+  [motifs smooth?]
+  (let [acids [\A \C \G \T]
+        n (count (first motifs))
+        t (float (count motifs))]
+    (zipmap acids (for [acid acids]
+                    (for [i (range n)
+                          :let [total (+ t (if smooth? 4 0))
+                                found (+ (count (filter (partial = acid) (map #(nth % i) motifs))) 
+                                         (if smooth? 1 0))]]
+                      (/ found total))))))
+
+(defn make-motifs
+  "the make motif step for greedy motif search"
+  [base-motif all-strands k]
+  (loop [motifs [base-motif]
+         strands all-strands]
+    (let [profile (gen-profile motifs true)]
+      (if (empty? strands) motifs
+        (recur (conj motifs (most-probable-kmer k (first strands) profile)) (rest strands))))))
+
+(defn greedy-motif-search
+  "Perform a greedy motif search that returns the best kmer motifs in the given dna strands"
+  [k strands]
+  (loop [best-motifs (map (partial take k) strands)
+         best-score (score best-motifs)
+         kmers (all-kmers k (first strands))]
+    (if (empty? kmers) best-motifs
+      (let [motifs (make-motifs (first kmers) (rest strands) k)
+            new-score (score motifs)]
+        (if (>= new-score best-score)
+          (recur best-motifs best-score (rest kmers))
+          (recur motifs new-score (rest kmers)))))))
--- test/bio_algs/motif_test.clj
+++ test/bio_algs/motif_test.clj
@@ -5,7 +5,70 @@
 (deftest motif-finding
   (testing "motif enumeration"
     (is (= #{"ATA" "ATT" "GTT" "TTT"}
-           (motif-enum [ "ATTTGGC" "TGCCTTA" "CGGTATC" "GAAAATT" ] 3 1))))) 
+           (motif-enum [ "ATTTGGC" "TGCCTTA" "CGGTATC" "GAAAATT" ] 3 1))))
+  (testing "score function"
+    (is (= 30 (score ["TCGGGGGTTTTT"
+                      "CCGGTGACTTAC"
+                      "ACGGGGATTTTC"
+                      "TTGGGGACTTTT"
+                      "AAGGGGACTTCC"
+                      "TTGGGGACTTCC"
+                      "TCGGGGATTCAT"
+                      "TCGGGGATTCCT"
+                      "TAGGGGAACTAC"
+                      "TCGGGTATAACC"]))))
+  (testing "distance function"
+    (is (= 5 (dist "AAA" ["TTACCTTAAC"
+                          "GATATCTGTC"
+                          "ACGGCGTTCG"
+                          "CCCTAAAGAG"
+                          "CGTCAGAGGT" ]))))
+  (testing "median string"
+    (is (= "GAC" (median-string 3 ["AAATTGACGCAT"
+                                   "GACGACCACGTT"
+                                   "CGTCAGCGCCTG"
+                                   "GCTGAGCACCGG"
+                                   "AGTACGGGACAG"]))))
+  (testing "Score kmer"
+    (is (= (* 0.4 0.3 0.5 0.2 0.4)
+           (score-kmer "CCGAG" {\A [0.2 0.2 0.3 0.2 0.3]
+                                \C [0.4 0.3 0.1 0.5 0.1]
+                                \G [0.3 0.3 0.5 0.2 0.4]
+                                \T [0.1 0.2 0.1 0.1 0.2]}))))
+  (testing "Making the profile"
+    (is (= {\A [0.2 0.2 0.3 0.2 0.3]
+            \C [0.4 0.3 0.1 0.5 0.1]
+            \G [0.3 0.3 0.5 0.2 0.4]
+            \T [0.1 0.2 0.1 0.1 0.2]}
+           (make-profile-from-string "0.2 0.2 0.3 0.2 0.3
+                                      0.4 0.3 0.1 0.5 0.1
+                                      0.3 0.3 0.5 0.2 0.4
+                                      0.1 0.2 0.1 0.1 0.2"))))
+  (testing "generating profiles"
+    (is (= {\A [0.75 1.0  0.0  0.0 ]
+            \C [0.0  0.0  0.25 0.5 ]
+            \G [0.0  0.0  0.0  0.25]
+            \T [0.25 0.0  0.75 0.25]}
+           (gen-profile ["AATC" "TATC" "AATG" "AACT"] false)))
+    (is (= {\A [0.5   0.625 0.125 0.125 ]
+            \C [0.125 0.125 0.25  0.375 ]
+            \G [0.125 0.125 0.125 0.25  ]
+            \T [0.25  0.125 0.5   0.25  ]}
+           (gen-profile ["AATC" "TATC" "AATG" "AACT"] true))))
+  (testing "most likely profile"
+    (is (= "CCGAG" (most-probable-kmer 5 
+                                       "ACCTGTTTATTGCCTAAGTTCCGAACAAACCCAATATAGCCCGAGGGCCT" 
+                                       {\A [0.2 0.2 0.3 0.2 0.3]
+                                        \C [0.4 0.3 0.1 0.5 0.1]
+                                        \G [0.3 0.3 0.5 0.2 0.4]
+                                        \T [0.1 0.2 0.1 0.1 0.2]}))))
+  (testing "greedy motif search"
+    (is (= (score ["CAG" "CAG" "CAA" "CAA" "CAA"])
+           (score (greedy-motif-search 3 ["GGCGTTCAGGCA"
+                                          "AAGAATCAGTCA"
+                                          "CAAGGAGTTCGC"
+                                          "CACGTCAATCAC"
+                                          "CAATAATATTCG"]))))))
 
 (deftest entropies
   (testing "calculating entropies"
@@ -16,18 +79,3 @@
   (testing "calculating motif entropies"
     (let [e (motif-entropy "ATCA" "ATGA" "ATTA" "ATAT")]
       (is (and (< 2.811 e) (> 2.812 e))))))
-
-(deftest medians
-  (testing "distance function"
-    (is (= 5 (dist "AAA" ["TTACCTTAAC"
-                          "GATATCTGTC"
-                          "ACGGCGTTCG"
-                          "CCCTAAAGAG"
-                          "CGTCAGAGGT" ])))
-    )
-  (testing "median string"
-    (is (= "GAC" (median-string 3 ["AAATTGACGCAT"
-                                   "GACGACCACGTT"
-                                   "CGTCAGCGCCTG"
-                                   "GCTGAGCACCGG"
-                                   "AGTACGGGACAG"])))))
