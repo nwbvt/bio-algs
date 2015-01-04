0e5e8f0f6e72d69145ceda68557d01690c84be7c
Nick Brown <nwbrown@gmail.com>
2015-01-04 18:55:33 -0500

    Faster find change algorithm with dynamic programming

--- src/bio_algs/dynprog.clj
+++ src/bio_algs/dynprog.clj
@@ -3,6 +3,8 @@
 (defn find-change
   "given a money value and a list of coins, return the minimum number of coins needed to make that amount"
   [value coins]
-  (if (zero? value) 0
-    (inc (apply min (for [coin coins :when (<= coin value)]
-                      (find-change (- value coin) coins))))))
+  (loop [min-nums [0] v 1]
+    (let [m (inc (apply min (for [c coins :when (<= c v)]
+                              (min-nums (- v c)))))]
+      (if (= value v) m
+        (recur (conj min-nums m) (inc v))))))
--- test/bio_algs/dynprog_test.clj
+++ test/bio_algs/dynprog_test.clj
@@ -4,4 +4,5 @@
 
 (deftest dyanmic-programming-functions
   (testing "does the find change function work correctly"
-    (is (= 2 (find-change 24 [50 12 10 5 1])))))
+    (is (= 2 (find-change 24 [50 12 10 5 1])))
+    (is (= 2 (find-change 40 [50 25 20 10 5 1])))))
