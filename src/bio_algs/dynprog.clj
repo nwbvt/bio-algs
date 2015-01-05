7abe1d53777a94638ba0edcb64f50ee71a227c42
Nick Brown <nwbrown@gmail.com>
2015-01-04 21:30:49 -0500

    implement manhattan solver

--- src/bio_algs/dynprog.clj
+++ src/bio_algs/dynprog.clj
@@ -1,4 +1,5 @@
-(ns bio-algs.dynprog)
+(ns bio-algs.dynprog
+  (:require [clojure.string :refer [split]]))
 
 (defn find-change
   "given a money value and a list of coins, return the minimum number of coins needed to make that amount"
@@ -8,3 +9,25 @@
                               (min-nums (- v c)))))]
       (if (= value v) m
         (recur (conj min-nums m) (inc v))))))
+
+(defn manhattan
+  "Finds the longest distance in the manhatten tourist problem"
+  [n m down right]
+  (let [s (atom {})
+        helper (fn [s i j]
+                 (let [d-val (if (zero? i) 0 (+ (nth (nth down (dec i)) j) (s [(dec i) j])))
+                      r-val (if (zero? j) 0 (+ (nth (nth right i) (dec j)) (s [i, (dec j)])))
+                      v (max d-val r-val)]
+                  (assoc s [i,j] v)))]
+    (doseq [i (range 0 (inc n)) j (range 0 (inc m))]
+      (swap! s helper i j))
+    (@s [n, m])))
+
+(defn read-in-manhattan
+  [file]
+  (let [make-ints (fn [strings] (map #(Integer/parseInt %) strings))
+        lines (map #(make-ints (split % #" ")) (split (slurp file) #"\n"))
+        [n, m] (first lines)
+        down (take n (drop 1 lines))
+        right (drop (inc n) lines)]
+    {:n n :m m :down down :right right}))
--- test/bio_algs/dynprog_test.clj
+++ test/bio_algs/dynprog_test.clj
@@ -5,4 +5,16 @@
 (deftest dyanmic-programming-functions
   (testing "does the find change function work correctly"
     (is (= 2 (find-change 24 [50 12 10 5 1])))
-    (is (= 2 (find-change 40 [50 25 20 10 5 1])))))
+    (is (= 2 (find-change 40 [50 25 20 10 5 1]))))
+  (testing "Manhattan Tourist Problem"
+    (is (= 34
+           (manhattan 4 4
+                      [[1 0 2 4 3]
+                       [4 6 5 2 1]
+                       [4 4 5 2 1]
+                       [5 6 8 5 3]]
+                      [[3 2 4 0]
+                       [3 2 4 2]
+                       [0 7 3 3]
+                       [3 3 0 2]
+                       [1 3 2 2]])))))
