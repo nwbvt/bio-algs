27588351118e90bb80b70fa760032a6ff4bded1d
Nicholas Brown <nwbrown@gmail.com>
2015-01-13 16:55:08 -0500

    greedy sorting for reversals

---
+++ src/bio_algs/rearrangements.clj
@@ -0,0 +1,31 @@
+(ns bio-algs.rearrangements
+  (:require [clojure.string :refer [join]]))
+
+(defn reversal
+  "Do a reversal of a permutation"
+  [perm start end]
+  (assert (not (neg? start)))
+  (assert (not (neg? end)))
+  (let [[left postfix] (split-at end perm)
+        [prefix inner] (split-at start left)
+        reversed (map - (reverse inner))]
+    (concat prefix reversed postfix)))
+
+(defn greedy-sorting
+  "Given a permutation, returns the sequence of permutations corresponding to the greedy sorting algorithm"
+  [initial]
+  (loop [perm initial perms [] i 1]
+    (if (> i (count initial)) perms
+      (if (= i (nth perm (dec i)))
+        (recur perm perms (inc i))
+        (let [index (max (.indexOf perm i) (.indexOf perm (- i))) ; the one that doesn't exist will be -1
+              _ (assert (not (neg? index)))
+              start (dec i)
+              end (inc index)
+              next-perm (reversal perm start end)]
+          (recur next-perm (conj perms next-perm) i))))))
+
+(defn format-perm
+  "Formats a permutation"
+  [perm]
+  (str "(" (join " " (map #(format "%+d" %)perm)) ")"))
---
+++ test/bio_algs/rearrangements_test.clj
@@ -0,0 +1,14 @@
+(ns bio-algs.rearrangements-test
+  (:require [clojure.test :refer :all]
+            [bio-algs.rearrangements :refer :all]))
+
+(deftest rearrangements
+  (testing "reversals"
+    (= [1 2 3 4 5 6] (reversal [1 2 3 -5 -4 6] 3 5)))
+  (testing "greedy sorting"
+    (is (= [[-1 -4 3 5 -2] [1 -4 3 5 -2] [1 2 -5 -3 4] [1 2 3 5 4] [1 2 3 -4 -5] [1 2 3 4 -5] [1 2 3 4 5]]
+           (greedy-sorting [-3 4 1 5 -2]))))
+  (testing "formatting permutations"
+    (is (= "(-1 -4 +3 +5 -2)" (format-perm [-1 -4 3 5 -2]) (format-perm '(-1 -4 3 5 -2))))
+    )
+  )
