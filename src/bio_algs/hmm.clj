9dbc2fd5b1961e675177f095897a2a07393d2a59
Nick Brown <nwbrown@gmail.com>
2015-06-07 17:25:10 -0400

    Viterbi method

--- src/bio_algs/hmm.clj
+++ src/bio_algs/hmm.clj
@@ -1,7 +1,63 @@
-(ns bio-algs.hmm)
+(ns bio-algs.hmm
+  (:require [bio-algs.core :refer :all]
+            [clojure.string :refer [split trim]]))
 
 (defn p-path
   [full-path trans-matrix]
   (loop [p 1/2 path full-path]
     (if (= 1 (count path)) p
       (recur (* p ((trans-matrix (first path)) (second path))) (rest path)))))
+
+(defn p-outcome
+  [outcome path emission-matrix]
+  (assert (= (count outcome) (count path)))
+  (reduce * (map (fn [[emission state]] ((emission-matrix state) emission))
+                 (partition 2 (interleave outcome path)))))
+
+(defn viterbi
+  "Uses the viterbi algorithm to find the optimal path"
+  [outcome states trans-matrix emission-matrix]
+  (let [cost (memoize (fn [state emission prior-state]
+                        (* (:cost prior-state)
+                           ((trans-matrix (:state prior-state)) state)
+                           ((emission-matrix state) emission))))
+        next-states (fn [emission previous-states]
+                      (zipmap states (map (fn [state]
+                                            (let [prior (previous-states
+                                                          (apply max-key
+                                                                 #(cost state emission
+                                                                        (previous-states %))
+                                                                 states))]
+                                              {:cost (cost state emission prior)
+                                               :state state
+                                               :path (conj (:path prior) state)})) states)))]
+    (loop [current (zipmap states
+                           (map (fn [state] {:cost 1 :state state :path []}) states))
+           emissions (seq outcome)]
+      (if (empty? emissions)
+        (let [best (apply max-key #((current %) :cost) states)]
+          (apply str ((current best) :path)))
+        (recur (next-states (first emissions) current) (rest emissions))))))
+
+(defn make-matrix
+  [matrix-strings]
+  (let [states (map first (split (trim (first matrix-strings)) #"[ \t]"))]
+    (loop [rows (rest matrix-strings)
+           matrix {}]
+      (if (empty? rows) matrix
+        (let [row (split (trim (first rows)) #"[ \t]")
+              row-sym (first (first row))
+              values (map #(Double/parseDouble %) (rest row))]
+          (recur (rest rows) (assoc matrix row-sym (zipmap states values))))))))
+
+(defn run
+  [input-file]
+  (let [in (vec (split (slurp input-file) #"\n"))
+        outcome (first in)
+        states (map first (split (in 4) #"[ \t]"))
+        matrix-lines (drop 8 in)
+        end-trans (+ 7 (count states))
+        trans-matrix (make-matrix (subvec in 6 end-trans))
+        emission-matrix (make-matrix (drop (inc end-trans) in))
+        ]
+    (viterbi outcome states trans-matrix emission-matrix)))
--- test/bio_algs/hmm_test.clj
+++ test/bio_algs/hmm_test.clj
@@ -2,6 +2,20 @@
   (:require [clojure.test :refer :all]
             [bio-algs.hmm :refer :all]))
 
+(deftest helper-tests
+  (testing "the generation of a transition matrix"
+    (is (= (make-matrix
+             ["	A	B" "A	0.377	0.623"	"B	0.26	0.74"])
+           {\A {\A 0.377 \B 0.623} \B {\A 0.26 \B 0.74}}))))
+
 (deftest hidden-paths
-  (is (> 1e-15 (- (p-path "ABABBBAAAA" {\A {\A 0.377 \B 0.623} \B {\A 0.26 \B 0.74}})
-                  0.000384928691755))))
+  (testing "finding the probability of a hidden path"
+    (is (> 1e-15 (- (p-path "ABABBBAAAA" {\A {\A 0.377 \B 0.623} \B {\A 0.26 \B 0.74}})
+                    0.000384928691755))))
+  (testing "find the probability of an emission given a path"
+    (is (> 1e-15 (- (p-outcome "zzzyxyyzzx" "BAAAAAAAAA" {\A {\x 0.176 \y 0.596 \z 0.228} \B {\x 0.225 \y 0.572 \z 0.203}})
+                    3.59748954746e-06))))
+  (testing "finding the optimal hidden path using the viterbi algorithm"
+    (is (= (viterbi "xyxzzxyxyy" [\A \B] {\A {\A 0.641 \B 0.359} \B {\A 0.729 \B 0.271}}
+                    {\A {\x 0.117 \y 0.691 \z 0.192} \B {\x 0.097 \y 0.42 \z 0.483}})
+           "AAABBAAAAA"))))
